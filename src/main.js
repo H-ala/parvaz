872daedcb8a4d8d306dc23b02e313e711aad2955
Hosein Alaei <hos.ala81@gmail.com>
2026-09-15 21:36:33 +0330

    add swiper to where to go section

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import './style.css'
+import Swiper from 'swiper'
+import 'swiper/css'
+import 'swiper/css/scrollbar'
+import 'swiper/css/navigation'
+import { Scrollbar } from 'swiper/modules'
+import { Navigation } from 'swiper/modules'
+
 import {
   createIcons,
   BadgeDollarSign,
@@ -14,7 +21,44 @@ import {
   ChevronLeft,
   ArrowLeftRight,
   Lock,
+  Flame,
+  Megaphone,
 } from 'lucide'
+
+const swiper = new Swiper('.mySwiper', {
+  modules: [Scrollbar, Navigation],
+  loop: true,
+
+  slidesPerView: 4,
+  spaceBetween: 24,
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 18,
+    },
+
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 18,
+    },
+  },
+})
 
 createIcons({
   icons: {
@@ -31,5 +75,7 @@ createIcons({
     ChevronLeft,
     ArrowLeftRight,
     Lock,
+    Flame,
+    Megaphone,
   },
 })
